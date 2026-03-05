665a16d070cc9b8e483c93fcdcf71b2949f4340d
christiansoliarski-cmyk <christian.soliarski@escola.pr.gov.br>
2026-03-05 11:49:51 -0300

    Create cont1.py

---
+++ conteudo/cont1.py
@@ -0,0 +1,4 @@
+# int para numeros ex: 7, 0, -4
+#float para números reais ou com virgula. Ex: 0.45 7,6
+#bool para valores lógicos de verdadeiro ou falso. Ex true ou false(sempre usar com letra maiuscula)
+#str para strings que sao palavras cracteres ou textos
