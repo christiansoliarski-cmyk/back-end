c476a9f991867d727066c970e5f1caeaeacc3768
christiansoliarski-cmyk <christian.soliarski@escola.pr.gov.br>
2026-03-11 11:41:00 -0300

    Create cont04.py

---
+++ conteudo/cont04.py
@@ -0,0 +1,7 @@
+O+ é utilizado como operador de adição
+O- é utilizado como sinal de subtração
+O* é utilizado para fazer multiplicação
+/ é utilzado para fazer a divisão
+** é utilizado para fazer exponenciação
+// é utilizado para fazer a divisão inteira
+% é utilizado para o resto da divisão
