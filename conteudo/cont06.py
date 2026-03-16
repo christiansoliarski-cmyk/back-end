fed94b26de1e9bba22b33a4004abfb8b265799aa
christiansoliarski-cmyk <christian.soliarski@escola.pr.gov.br>
2026-03-16 10:57:49 -0300

    cont06.py

---
+++ conteudo/cont06.py
@@ -0,0 +1,11 @@
+#alinhamento de mascara
+#podemos alinhar o nome dentro do numero de caracteres que desejamos
+#alinhamento para direita
+#nome=input ('qual seu nome?')
+#print('prazer em conhece-lo nome {:>20}!.format (nome)')
+#alinhamento para esquerda
+##nome=input ('qual seu nome?')
+#print('prazer em conhece-lo nome {:<20}!.format (nome)')
+#alinhamento para o centro
+#nome=input ('qual seu nome?')
+#print('prazer em conhece-lo nome {:^20}!.format (nome)')
