a24df350097cb18503acddc7f83c1be4ceb96327
christiansoliarski-cmyk <christian.soliarski@escola.pr.gov.br>
2026-03-16 10:50:09 -0300

    cont05.py

---
+++ conteudo/cont05.py
@@ -0,0 +1,8 @@
+#5+3*2==
+#3*5+4**2=
+#3(5+4)**2=
+#O python é utilizado em calculos cientificios é o tamanho da memoria ou seja conseguimos chegar a numeros extremamentes grandes
+#Para exponenciação, podemos usar o pow(x, y) onde o numero x é elevado ao numero y
+#para raiz quadrada podemos usar o **(1/2). elevando o numero a 1/2 nos obtemos a raiz quadrada, para raiz cubica **(1/3). e assim por diante.
+#podemos usar os operadores em string
+#ex '='*5 vai ficar '====='
